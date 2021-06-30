79c95da372d15161769d6b365a82a7eb6530aefb
Sandesh <sandeshsg6@gmail.com>
2021-06-30 19:54:22 +0530

    added url atrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ivote/App/routes.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(IVote());
 }
 
